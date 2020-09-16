a338e685daa8806d41f68972082447ca3f4f1a86
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2020-09-15 23:25:43 -0700

    Add website logo and change fonts.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="top-nav">
+        <img id="header-background" src="https://i.pinimg.com/originals/b8/8e/59/b88e5919775afd0de88efa8ebcaf1250.png" alt=""></img>
+        <header className="welcome">Hi, welcome to my blog :)</header>
+      </div>
     </div>
   );
 }

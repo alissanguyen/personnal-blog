ce32c6bf16e7a444280444e60a220a22eb2448bb
Alissa Nguyen <alissa.nguyen1211@gmail.com>
2020-09-18 00:00:12 -0700

    Add Contact button.

--- src/App.js
+++ src/App.js
@@ -7,12 +7,19 @@ function App() {
       <div className="top-nav">
         <div className="top-top-nav">
           <button className="home-button">Home</button>
+          <button className="contact-button">Contact</button>
         </div>
         <div className="header-background">
           <header className="welcome">Hi, welcome to my blog :)</header>
         </div>
       </div>
       <div className="blog-content">
+      <ul className="individual-blog"></ul>
+      <ul className="individual-blog"></ul>
+      <ul className="individual-blog"></ul>
+      <ul className="individual-blog"></ul>
+      <ul className="individual-blog"></ul>
+      <ul className="individual-blog"></ul>
       <ul className="individual-blog"></ul>
       <ul className="individual-blog"></ul>
       <ul className="individual-blog"></ul>
